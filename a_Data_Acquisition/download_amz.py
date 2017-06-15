4f3f89db09d2b84f3c6d5168211f7d8462c83ce9
timonius <timon.weitkamp@wur.nl>
2017-06-15 12:05:26 +0200

    function dowload_amz

--- a_Data_Acquisition/download_amz.py
+++ a_Data_Acquisition/download_amz.py
@@ -6,9 +6,11 @@ import os
 import sentinelhub
 import time
 
-def dowload_amz(list)
+def dowload_amz(product_names)
+    owd = os.getcwd() #original working directory (owd)
     new_dir = 'Data/%s'%time.strftime('%a%d%b%Y%H%M')
     os.mkdir(new_dir)
     os.chdir(new_dir)
-    for element in list:
+    for element in product_names:
         sentinelhub.download_safe_format(element)
+    os.chdir(owd)

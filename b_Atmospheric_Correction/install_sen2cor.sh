8fd0aa16296afa0bb6bd6f04dd4e10abda14fe73
keesbaake <kees.baake@gmail.com>
2017-06-16 11:18:37 +0200

    Added lines for further installation of other needed files/programs

--- b_Atmospheric_Correction/install_sen2cor.sh
+++ b_Atmospheric_Correction/install_sen2cor.sh
@@ -37,3 +37,40 @@ which python # directory checker
 
 
 
+
+
+
+
+
+
+## Change Underlying lines to install in correct directories (also fix most current versions)
+mkdir /home/user/SNAP
+cd /home/user/SNAP
+wget http://step.esa.int/downloads/5.0/installers/esa-snap_all_unix_5_0.sh
+sudo sh esa-snap_all_unix_5_0.sh -c
+
+python path: /home/user/anaconda2/bin/python
+
+
+----------------------------------------
+
+mkdir /home/user/SEN2COR
+cd /home/user/SEN2COR
+cd /home/user/SEN2COR
+wget http://step.esa.int/thirdparties/sen2cor/2.3.1/sen2cor-2.3.1.tar.gz
+tar xvzf sen2cor-2.3.1.tar.gz
+cd sen2cor-2.3.1
+
+python setup.py install
+
+----------------------------------------
+
+Set variables:
+
+sudo nano /etc/bash.bashrc
+
+add the following lines at the end of the doc , save and quit
+
+export SEN2COR_HOME=/home/user/sen2cor
+export SEN2COR_BIN=/home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor
+export GDAL_DATA=/home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/gdal_data

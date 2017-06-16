c60a16687443f24df54f5b45e83f2f7e1a8d01d9
keesbaake <kees.baake@gmail.com>
2017-06-16 15:48:18 +0200

    Erasing all redundant lines

--- b_Atmospheric_Correction/install_sen2cor.sh
+++ b_Atmospheric_Correction/install_sen2cor.sh
@@ -1,42 +1,23 @@
 #!/usr/bin/env bash
 # Go to home directory
-cd ~
+cd $HOME
 
 # PRELIMINARIES
 # Setup download folder, assuming data folder is inside user
-mkdir /home/user/DL_temp
+mkdir $HOME/DL_temp
 
-## 1. INSTALL ANACONDA
-
-# You can change what anaconda version you want at
-# https://repo.continuum.io/archive/
-wget https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh
-bash Anaconda2-4.2.0-Linux-x86_64.sh -b -p ~/anaconda
-rm Anaconda2-4.2.0-Linux-x86_64.sh
-echo 'export PATH="~/anaconda2/bin:$PATH"' >> ~/.bashrc
-
-# Updating to latest
-conda update conda
-
-
-# Alternate script
+## 1. DOWNLOAD AND INSTALL LATEST ANACONDA (PYTHON 2.7)
 CONTREPO=https://repo.continuum.io/archive/
-# Stepwise filtering of the html at $CONTREPO
-# Get the topmost line that matches our requirements, extract the file name.
 ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda2-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
-wget -O ~/DL_temp/anaconda.sh $CONTREPO$ANACONDAURL
-bash ~/DL_temp/anaconda.sh
-
+wget -O $HOME/DL_temp/anaconda.sh $CONTREPO$ANACONDAURL
+bash $HOME/DL_temp/anaconda.sh
 
 ## 2. CHECK PYTHON DIRECTORY
 conda search "^python$" #version checker
 conda info --envs #environment checker
 which python # directory checker
 
-## 3. INSTALL SEN2COR
-
-
-# Download latest version from source
+## 3. DOWNLOAD AND INSTALL SEN2COR
 SEN2REPO=http://step.esa.int/thirdparties/sen2cor/
 SEN2VERSION=$(wget -q -O - $SEN2REPO | grep "$(date +%Y)" | tail -n 1 | cut -d \" -f 8)
 SEN2SITE=$SEN2REPO$SEN2VERSION
@@ -44,13 +25,9 @@ SEN2URL=$(wget -q -O - $SEN2SITE | grep "tar" | head -n 1 | cut -d \" -f 8)
 wget -O ~/Downloads/sen2cor.tar.gz $SEN2SITE$SEN2URL
 bash mkdir /home/user/SEN2COR
 bash tar -xzvf ~/Downloads/sen2cor.tar.gz -C $HOME
-
-# Install
-bash cd $HOME/sen2cor-2.3.1
-python setup.py install
+python setup.py install -C /home/user/sen2cor-2.3.1
 
 # Set environment variables
-
 bash sudo nano /etc/bash.bashrc_profile
 
 add the following lines at the end of the doc , save and quit
@@ -59,34 +36,9 @@ export SEN2COR_HOME=/home/user/sen2cor
 export SEN2COR_BIN=/home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor
 export GDAL_DATA=/home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/gdal_data
 
-
-## Install latest version of SNAP
+## DOWNLOAD AND INSTALL LATEST SNAP
 SNAPREPO=http://step.esa.int/downloads/
 SNAPVERSION=$(wget -q -O - $SNAPREPO| grep "$(date +%Y)" | grep -v ".txt" | tail -n 1 | cut -d \" -f 8)
 SNAPINSTALLDIR=$(wget -q -O - $SNAPREPO$SNAPVERSION | grep "installers" | tail -n 1 | cut -d \" -f 8)
 SNAPFILE=$(wget -q -O - $SNAPREPO$SNAPVERSION$SNAPINSTALLDIR | grep "all" | grep "unix" |  tail -n 1 | cut -d \" -f 8)
 wget -O ~/Downloads/snap.sh $SNAPREPO$SNAPVERSION$SNAPINSTALLDIR$SNAPFILE
-
-
-## Change Underlying lines to install in correct directories (also fix most current versions)
-cd /home/user/SNAP
-wget http://step.esa.int/downloads/5.0/installers/esa-snap_all_unix_5_0.sh
-sudo sh esa-snap_all_unix_5_0.sh -c
-
-python path: /home/user/anaconda2/bin/python
-
-
-----------------------------------------
-
-mkdir /home/user/SEN2COR
-cd /home/user/SEN2COR
-cd /home/user/SEN2COR
-wget http://step.esa.int/thirdparties/sen2cor/2.3.1/sen2cor-2.3.1.tar.gz
-tar xvzf sen2cor-2.3.1.tar.gz
-cd sen2cor-2.3.1
-
-python setup.py install
-
-----------------------------------------
-
-

3d210dbd906df48f5aeca04fa0b593db46ddd9f2
arnanaraza <arnan.araza@wur.nl>
2017-06-16 05:04:43 -0400

    update #3

--- b_Atmospheric_Correction/install_sen2cor.sh
+++ b_Atmospheric_Correction/install_sen2cor.sh
@@ -2,6 +2,8 @@
 # Go to home directory
 cd ~
 
+## 1. INSTALL ANACONDA
+
 # You can change what anaconda version you want at
 # https://repo.continuum.io/archive/
 wget https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh
@@ -9,7 +11,29 @@ bash Anaconda2-4.2.0-Linux-x86_64.sh -b -p ~/anaconda
 rm Anaconda2-4.2.0-Linux-x86_64.sh
 echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bashrc
 
-# Refresh basically
-source .bashrc
-
+# Updating to latest
 conda update conda
+
+$ source ~/.profile
+
+# Alternate script
+CONTREPO=https://repo.continuum.io/archive/
+# Stepwise filtering of the html at $CONTREPO
+# Get the topmost line that matches our requirements, extract the file name.
+ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda2-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
+wget -O ~/Downloads/anaconda.sh $CONTREPO$ANACONDAURL
+bash ~/Downloads/anaconda.sh
+
+
+## 2. CHECK PYTHON DIRECTORY
+
+conda search "^python$" #version checker
+conda info --envs #environment checker
+which python # directory checker
+
+## 3. INSTALL SEN2COR
+
+# Download from source
+
+
+

64751cf57b2abe16ebc88783f350c95e11989e64
keesbaake <kees.baake@gmail.com>
2017-06-19 11:34:08 +0200

    Variable directory paths added and cleaned up some warning messages.

--- b_Atmospheric_Correction/install_sen2cor.sh
+++ b_Atmospheric_Correction/install_sen2cor.sh
@@ -6,65 +6,60 @@ Existing user settings will be overwritten.
 Are you sure you want to install  this script? [yes|no]
 >>> "
 read ans
-while [[ ($ans != "yes") && ($ans != "Yes") && ($ans != "YES") &&
-($ans != "no") && ($ans != "No") && ($ans != "NO") ]]
+while [[ (${ans} != "yes") && (${ans} != "Yes") && (${ans} != "YES") && (${ans} != "no") && (${ans} != "No") && (${ans} != "NO") ]]
 do
 echo -n "Please answer 'yes' or 'no':
 >>> "
 read ans
 done
-if [[ ($ans != "yes") && ($ans != "Yes") && ($ans != "YES") ]]
+if [[ (${ans} != "yes") && (${ans} != "Yes") && (${ans} != "YES") ]]
 then
 echo "The install agreement wasn't approved, aborting installation."
 exit 2
 fi
 
-# Go to home directory
-cd $HOME
+# 0. DEFINE DIRECTORY VARIABLES
+defdir=$HOME
+cd ${defdir}
+mkdir ${defdir}/DL_temp
+dldir=${defdir}/DL_temp
 
-# PRELIMINARIES
-# Setup download folder, assuming data folder is inside user
-mkdir $HOME/DL_temp
-
-## 1. DOWNLOAD AND INSTALL LATEST ANACONDA (PYTHON 2.7)
+## 1. DOWNLOAD AND INSTALL LATEST ANACONDA (FOR PYTHON 2.7)
 CONTREPO=https://repo.continuum.io/archive/
-ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda2-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
-wget -O $HOME/DL_temp/anaconda.sh $CONTREPO$ANACONDAURL
-bash $HOME/DL_temp/anaconda.sh -b #uses the default settings
-
+ANACONDAURL=$(wget -q -O - ${CONTREPO} index.html | grep "Anaconda2-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
+wget -O ${dldir}/anaconda.sh ${CONTREPO}${ANACONDAURL}
+bash ${dldir}/anaconda.sh -b #uses the default settings
 
 ## 2. CHECK PYTHON DIRECTORY
-bash conda search "^python$" #version checker
-bash conda info --envs #environment checker
-bash which python # directory checker
+conda search "^python$" #version checker
+conda info --envs #environment checker
+which python # directory checker
 
-## 3. DOWNLOAD AND INSTALL SEN2COR
-# Get latest version
+## 3. DOWNLOAD AND INSTALL LATEST SEN2COR
 SEN2REPO=http://step.esa.int/thirdparties/sen2cor/
-SEN2VERSION=$(wget -q -O - $SEN2REPO | grep "$(date +%Y)" | tail -n 1 | cut -d \" -f 8)
-SEN2SITE=$SEN2REPO$SEN2VERSION
-SEN2URL=$(wget -q -O - $SEN2SITE | grep "tar" | head -n 1 | cut -d \" -f 8)
-wget -O $HOME/DL_temp/sen2cor.tar.gz $SEN2SITE$SEN2URL
-bash mkdir $HOME/SEN2COR -p
-bash tar -xzvf ~/DL_temp/sen2cor.tar.gz -C $HOME/SEN2COR/
-bash cd $HOME/SEN2COR/sen2cor-2.3.1/
-python setup.py install
-cp -rf /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/ /$HOME/
-
-
+SEN2VERSION=$(wget -q -O - ${SEN2REPO} | grep "$(date +%Y)" | tail -n 1 | cut -d \" -f 8)
+SEN2SITE=${SEN2REPO}${SEN2VERSION}
+SEN2URL=$(wget -q -O - ${SEN2SITE} | grep "tar" | head -n 1 | cut -d \" -f 8)
+wget -O $HOME/DL_temp/sen2cor.tar.gz ${SEN2SITE}${SEN2URL}
+mkdir ${defdir}/SEN2COR -p
+tar -xzvf ${dldir}/sen2cor.tar.gz -C ${defdir}/SEN2COR/
+cd ${defdir}/SEN2COR/sen2cor-2.3.1/
+yes yes | python setup.py install
+cp -rf ${defdir}/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/ ${dldir}
 
 # Set environment variables
-bash sudo nano /etc/bash.bashrc_profile
-
-add the following lines at the end of the doc , save and quit
-
+sudo nano /etc/bash.bashrc_profile
+## add the following lines at the end of the doc , save and quit
 export SEN2COR_HOME=/home/user/sen2cor
 export SEN2COR_BIN=/home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor
 export GDAL_DATA=/home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/gdal_data
 
-## DOWNLOAD AND INSTALL LATEST SNAP
+## 4. DOWNLOAD AND INSTALL SEN2THREE
+
+## 5. DOWNLOAD AND INSTALL LATEST SNAP
 SNAPREPO=http://step.esa.int/downloads/
-SNAPVERSION=$(wget -q -O - $SNAPREPO| grep "$(date +%Y)" | grep -v ".txt" | tail -n 1 | cut -d \" -f 8)
-SNAPINSTALLDIR=$(wget -q -O - $SNAPREPO$SNAPVERSION | grep "installers" | tail -n 1 | cut -d \" -f 8)
-SNAPFILE=$(wget -q -O - $SNAPREPO$SNAPVERSION$SNAPINSTALLDIR | grep "all" | grep "unix" |  tail -n 1 | cut -d \" -f 8)
-wget -O ~/Downloads/snap.sh $SNAPREPO$SNAPVERSION$SNAPINSTALLDIR$SNAPFILE
+SNAPVERSION=$(wget -q -O - ${SNAPREPO} | grep "$(date +%Y)" | grep -v ".txt" | tail -n 1 | cut -d \" -f 8)
+SNAPINSTALLDIR=$(wget -q -O - ${SNAPREPO}${SNAPVERSION} | grep "installers" | tail -n 1 | cut -d \" -f 8)
+SNAPFILE=$(wget -q -O - ${SNAPREPO}${SNAPVERSION}${SNAPINSTALLDIR} | grep "all" | grep "unix" |  tail -n 1 | cut -d \" -f 8)
+wget -O ~/Downloads/snap.sh ${SNAPREPO}${SNAPVERSION}${SNAPINSTALLDIR}${SNAPFILE}
+

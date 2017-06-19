6159d1cdebb4beac7ca3d252b3e20a0859878302
keesbaake <kees.baake@gmail.com>
2017-06-19 12:00:44 +0200

    Variable directory paths added and cleaned up some warning messages.

--- b_Atmospheric_Correction/install_sen2cor.sh
+++ b_Atmospheric_Correction/install_sen2cor.sh
@@ -36,11 +36,11 @@ conda info --envs #environment checker
 which python # directory checker
 
 ## 3. DOWNLOAD AND INSTALL LATEST SEN2COR
-SEN2REPO=http://step.esa.int/thirdparties/sen2cor/
-SEN2VERSION=$(wget -q -O - ${SEN2REPO} | grep "$(date +%Y)" | tail -n 1 | cut -d \" -f 8)
-SEN2SITE=${SEN2REPO}${SEN2VERSION}
+SEN2CREPO=http://step.esa.int/thirdparties/sen2cor/
+SEN2VERSION=$(wget -q -O - ${SEN2CREPO} | grep -v -i ¨SEN¨ | grep "$(date +%Y)" | tail -n 1 | cut -d \" -f 8)
+SEN2SITE=${SEN2CREPO}${SEN2VERSION}
 SEN2URL=$(wget -q -O - ${SEN2SITE} | grep "tar" | head -n 1 | cut -d \" -f 8)
-wget -O $HOME/DL_temp/sen2cor.tar.gz ${SEN2SITE}${SEN2URL}
+wget -O ${dldir}/sen2cor.tar.gz ${SEN2SITE}${SEN2URL}
 mkdir ${defdir}/SEN2COR -p
 tar -xzvf ${dldir}/sen2cor.tar.gz -C ${defdir}/SEN2COR/
 cd ${defdir}/SEN2COR/sen2cor-2.3.1/
@@ -55,6 +55,8 @@ export SEN2COR_BIN=/home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.
 export GDAL_DATA=/home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/gdal_data
 
 ## 4. DOWNLOAD AND INSTALL SEN2THREE
+SEN2TREPO=http://step.esa.int/thirdparties/sen2three/
+SEN2TVERSION=$(wget -q -O - ${SEN2tREPO} | grep "$(date +%Y)" | tail -n 1 | cut -d \" -f 8)
 
 ## 5. DOWNLOAD AND INSTALL LATEST SNAP
 SNAPREPO=http://step.esa.int/downloads/

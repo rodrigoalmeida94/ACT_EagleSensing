c0703b01b5e10904c6a0cb5930a76f7fa7adb4fd
keesbaake <kees.baake@gmail.com>
2017-06-20 17:05:49 +0200

    fmask download update

--- z_Install_Software/fmask_install.sh
+++ z_Install_Software/fmask_install.sh
@@ -5,7 +5,7 @@ dldir=${defdir}/DL_temp
 FMASKREPO=https://bitbucket.org/chchrsc/python-fmask/downloads/
 FMASKFILE=$(wget -q -O - ${FMASKREPO} | egrep '[[:alnum:]]\.[[:alnum:]]\.[[:alnum:]].tar.gz' | head -n 1 | cut -d \" -f 2)
 wget -O ${dldir}/fmask.tar.gz https://bitbucket.org$FMASKFILE
-sudo t
+sudo tar -xzvf ${dldir}/fmask.tar.gz -C ${defdir}/FMASK/
 
-cd python-fmask-0.4.4
+cd ${defdir}/FMASK
 python setup.py build

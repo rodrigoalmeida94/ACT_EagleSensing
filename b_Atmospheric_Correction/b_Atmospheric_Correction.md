ac8b1b7dc7cc5c225cc3a1f874738457e2de8b34
timonius <timon.weitkamp@wur.nl>
2017-06-20 17:18:51 +0200

    first attempt to paralell batch process L1c

--- b_Atmospheric_Correction/b_Atmospheric_Correction.md
+++ b_Atmospheric_Correction/b_Atmospheric_Correction.md
@@ -41,6 +41,7 @@ Calling sen2cor from python script:
  
  
  Raw commands: 
- L2A_Process --resolution=10 --GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml /home/user/sen2data/S2A_MSIL1C_20170103T022102_N0204_R003_T51PUR_20170103T023326.SAFE
+  L2A_Process --resolution=10 --GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml /home/user/sen2data/S2A_MSIL1C_20170103T022102_N0204_R003_T51PUR_20170103T023326.SAFE
+
 
 
--- b_Atmospheric_Correction/run_sen2cor.py
+++ b_Atmospheric_Correction/run_sen2cor.py
@@ -7,10 +7,7 @@ import os.path
 
 #Check directory of L1C images, rename if necessary
 
-os.getcwd()
-datadir = r'/home/user/sen2data/'
-if not os.path.exists(datadir):
-    os.makedirs(datadir)
+datadir = r'/media/sf_M_DRIVE/ACT_EagleSensing/a_Data_Acquisition/Data/hubTue20Jun20171214/'
 os.chdir(datadir)
 print "The files in the data folder is/are: %s"%os.listdir(os.getcwd())
 for file in os.listdir(datadir):
@@ -23,13 +20,23 @@ for file in os.listdir(datadir):
 
 #Run sen2cor
 
-L1C = os.listdir(os.getcwd())[1]
-res = []
+#get only L1C folders and batch process it
+datafiles = os.listdir(os.getcwd())
+checker = "L1C"
 def run_sen2cor (res):
-    runit = os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(datadir) + str(L1C))
-    return (runit)
+    if datafiles != 0:
+        for i in datafiles:
+            if i[7:10] == str(checker):
+                os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(datadir) + str(datafiles[0]))
+                os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(datadir) + str(datafiles[1]))
+                os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(datadir) + str(datafiles[2]))
+                os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(datadir) + str(datafiles[3]))
+                # should have some trick to parallel process
+    else:
+        print "folder has no files yet"
 
-run_sen2cor (10)
+run_sen2cor (20)
+
 
 
 

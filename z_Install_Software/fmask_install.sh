6f850fda8081acbaf7578844267e874f5a8b4e24
keesbaake <kees.baake@gmail.com>
2017-06-20 17:03:40 +0200

    fix SEN2TVERSION

--- z_Install_Software/fmask_install.sh
+++ z_Install_Software/fmask_install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+defdir=$HOME
+dldir=${defdir}/DL_temp
 
 FMASKREPO=https://bitbucket.org/chchrsc/python-fmask/downloads/
 FMASKFILE=$(wget -q -O - ${FMASKREPO} | egrep '[[:alnum:]]\.[[:alnum:]]\.[[:alnum:]].tar.gz' | head -n 1 | cut -d \" -f 2)
-wget -O ${dldir}/fmask.tar.gz $FMASKREPO$FMASKVERSION
-
+wget -O ${dldir}/fmask.tar.gz https://bitbucket.org$FMASKFILE
+sudo t
 
 cd python-fmask-0.4.4
 python setup.py build

330b57b2b7f8ec6f5901e5d0fa5bc459843a1531
arnanaraza <arnan.araza@wur.nl>
2017-06-21 02:52:19 -0400

    update #2 -function (with resolution and directory as arguments) to check L1C files and run sen2cor

--- b_Atmospheric_Correction/run_sen2cor.py
+++ b_Atmospheric_Correction/run_sen2cor.py
@@ -15,8 +15,8 @@ print "The files in the data folder is/are: %s"%os.listdir(os.getcwd())
 #Get only L1C folders with .SAFE and process it
 datafiles = os.listdir(os.getcwd())
 checker = "L1C"
-run = []
 def run_sen2cor (res, dir):
+    run = []
     for i in datafiles:
         if i[7:10] == str(checker) and i.endswith(".SAFE"):
             run = os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(dir) + str(datafiles[0]))

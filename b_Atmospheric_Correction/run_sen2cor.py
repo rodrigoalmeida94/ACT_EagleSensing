439725b14c9ed3d023c1a8a0edbfe04b3b0c7adb
arnanaraza <arnan.araza@wur.nl>
2017-06-21 02:49:48 -0400

    update #2 -function (with resolution and directory as arguments) to check L1C files and run sen2cor

--- b_Atmospheric_Correction/run_sen2cor.py
+++ b_Atmospheric_Correction/run_sen2cor.py
@@ -1,41 +1,30 @@
-#Function to check L1C sourcefile, format properly (assuming original data file name with missing .SAFE), and run sen2cor
+#Function to check L1C sourcefile, format properly, and run sen2cor
 
 import os
 import os.path
 
 
-
-#Check directory of L1C images, rename if necessary
+## Directory and L1C folder check
 
 datadir = r'/media/sf_M_DRIVE/ACT_EagleSensing/a_Data_Acquisition/Data/hubTue20Jun20171214/'
 os.chdir(datadir)
 print "The files in the data folder is/are: %s"%os.listdir(os.getcwd())
-for file in os.listdir(datadir):
-    if file.endswith(".SAFE"):
-        print "already formatted"
-    else:
-        os.rename(file,(file + ".SAFE"))
 
+## Run sen2cor
 
-
-#Run sen2cor
-
-#get only L1C folders and batch process it
+#Get only L1C folders with .SAFE and process it
 datafiles = os.listdir(os.getcwd())
 checker = "L1C"
-def run_sen2cor (res):
-    if datafiles != 0:
-        for i in datafiles:
-            if i[7:10] == str(checker):
-                os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(datadir) + str(datafiles[0]))
-                os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(datadir) + str(datafiles[1]))
-                os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(datadir) + str(datafiles[2]))
-                os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(datadir) + str(datafiles[3]))
-                # should have some trick to parallel process
-    else:
-        print "folder has no files yet"
+run = []
+def run_sen2cor (res, dir):
+    for i in datafiles:
+        if i[7:10] == str(checker) and i.endswith(".SAFE"):
+            run = os.system ("L2A_Process --resolution="+ str(res) + " " + "--GIP_L2A /home/user/anaconda2/lib/python2.7/site-packages/sen2cor-2.3.1-py2.7.egg/sen2cor/cfg/L2A_GIPP.xml" + " " + str(dir) + str(datafiles[0]))
+        else:
+            os.rename(i, (i + ".SAFE"))
+    return run
 
-run_sen2cor (20)
+run_sen2cor (20, datadir)
 
 
 

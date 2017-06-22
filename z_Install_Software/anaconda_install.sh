1e849cd2e11b8284ed9b6f443b352e69a4f0d88d
arnanaraza <arnan.araza@wur.nl>
2017-06-22 08:21:55 -0400

    Mosaic gdal install fix

--- z_Install_Software/anaconda_install.sh
+++ z_Install_Software/anaconda_install.sh
@@ -9,8 +9,9 @@ profilenondebian=$HOME/.bashrc
 cd $HOME
 
 ## 1. DOWNLOAD AND INSTALL ANACONDA (FOR PYTHON 2.7)
-#CONTREPO=https://repo.continuum.io/archive/
+CONTREPO=https://repo.continuum.io/archive/
 #ANACONDAURL=$(wget -q -O - ${CONTREPO} index.html | grep "Anaconda2-" | grep "Linux" | grep "86_64" | grep "2.4.2" | head -n 1 | cut -d \" -f 2)
+#The anacondaurl directly above (line 13) is a specific anaconda version instead of the latest
 ANACONDAURL=$(wget -q -O - ${CONTREPO} index.html | grep "Anaconda2-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
 wget -O ${dldir}/anaconda.sh ${CONTREPO}${ANACONDAURL}
 cd ${dldir}

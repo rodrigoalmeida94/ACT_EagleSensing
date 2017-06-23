a320451d5260e2e0d9d8a95d44fd4ff04d291de2
arnanaraza <arnan.araza@wur.nl>
2017-06-23 10:40:43 -0400

    -will address overwriting issue this weekend

--- b_Atmospheric_Correction/organize_L1C_L2A.py
+++ b_Atmospheric_Correction/organize_L1C_L2A.py
@@ -8,9 +8,10 @@ datadir_L1C = r'/media/sf_M_DRIVE/L1C' #modify own directory
 print "The files in the data folder is/are: %s"%os.listdir(os.getcwd())
 
 ## Make/set directory of L2A files and move files
-checker1 = "L2A"
-datadir_L2A = r'/media/sf_M_DRIVE/L2A' #modify own directory
+
 def folder_arrange (dir_L1C, dir_L2A):
+
+    checker1 = "L2A"
     dfiles = os.listdir(dir_L1C)
     if not os.path.exists(dir_L2A):
         os.makedirs(dir_L2A)
@@ -21,5 +22,7 @@ def folder_arrange (dir_L1C, dir_L2A):
         else:
             print "no L2A folders yet"
 
-folder_arrange(datadir_L1C, datadir_L2A)
+datadir_L1C = r'/media/sf_M_DRIVE/L1C'
+datadir_L2A = r'/media/sf_M_DRIVE/L2A'  # modify own directory
 
+folder_arrange(datadir_L1C, datadir_L2A)
--- b_Atmospheric_Correction/run_sen2cor.py
+++ b_Atmospheric_Correction/run_sen2cor.py
@@ -1,24 +1,20 @@
 #Function to check L1C sourcefile, format properly, and run sen2cor
 
 import os
-import os.path
 from multiprocessing import Pool
 from itertools import starmap
 import parmap
-
-## Directory and L1C folder check
-datadir_L1C = r'/media/sf_M_DRIVE/L1C' #modify own directory
-os.chdir(datadir_L1C)
-print "The files in the data folder is/are: %s"%os.listdir(os.getcwd())
+import fire
 
 
-## RUN SEN2COR--------------------------------
+## RUN SEN2COR---------------------------------------------------------
 
 # 1. ONE BY ONE PROCESSING
-datafiles = os.listdir(os.getcwd())
-checker = "L1C"
+
 def run_sen2cor (res, dir): #Gets only L1C folders with .SAFE and process it
-    run = []
+    os.chdir(dir)
+    datafiles = os.listdir(os.getcwd())
+    checker = "L1C"
     for folders in datafiles:
         if folders[7:10] == checker and folders.endswith(".SAFE"):
             tfile = folders
@@ -30,15 +26,20 @@ def run_sen2cor (res, dir): #Gets only L1C folders with .SAFE and process it
 
 
 # 2. BATCH PROCESSING
+
 def sen2_batch (res, dir): # Creates a list of arguments based on number of files to run
+    os.chdir(dir)
+    datafiles = os.listdir(os.getcwd())
     multiplier = len(datafiles)
     slist = [(res, dir)]*multiplier
-    pool = Pool(6) #adjust accordingly depending on computer processor capacity and files to be run
-    parmap.starmap(run_sen2cor, slist, parallel=True, pool=pool, processes=multiplier)
+    pool = Pool(2) #adjust accordingly depending on computer processor capacity and files to be run
+    parmap.starmap(run_sen2cor, slist, pool=pool)
 
+datadir_L1C = r'/media/sf_M_DRIVE/L1C' #sample directory
 sen2_batch (10, datadir_L1C)
+
 
 ## Delete unfinished L2A folder always!
 
-
-----------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    fire.Fire(sen2_batch)

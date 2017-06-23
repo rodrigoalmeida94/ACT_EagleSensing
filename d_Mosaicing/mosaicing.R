46d3c2e2e11604383c588f9db72b7746e8f0089d
rodrigoalmeida94 <rodrigo.almeida@wur.nl>
2017-06-23 17:26:20 +0200

    Added comments

--- d_Mosaicing/mosaicing.R
+++ d_Mosaicing/mosaicing.R
@@ -5,12 +5,12 @@
 ## DANGER
 setwd("/media/sf_D_DRIVE/hubTue20Jun20171214/L2A/")
 
+# Conda environment as well? Or just install packages?
 library(rgdal)
 library(raster)
 library(XML)
 
 # Handles arguments passed by CLI
-
 args = commandArgs(trailingOnly=TRUE)
 #Rscript --vanilla mosaicing.R input_dir output_dir resolution?
 
@@ -18,15 +18,18 @@ args = commandArgs(trailingOnly=TRUE)
 if (length(args)!=2) {
   stop("Two arguments must be supplied (input directory and output directory).", call.=FALSE)
 }
-owd <- getwd()
-setwd(args[0])
 
-# Gel list of products in the directory
+owd <- getwd()
+
+setwd(args[0]) # Directory inputed by the user
+# Get list of products in the directory
 products <- dir(pattern='*.SAFE')
 
+# Error if no products are present/directory doesn't exist
+
+# Load products into VRT, save dates from metadata
 products_vrt <- c()
 dates_products <- c()
-
 for(elem in products) {
   # DANGER RESOLUTION
   granule = dir(paste0(elem,'/GRANULE/'))
@@ -38,10 +41,11 @@ for(elem in products) {
   date <- xmlValue(xml_meta[[1]][[1]][[1]])
   dates_products <- c(dates_products,date)
 }
-
 rm(elem, granule, granule_dir,output_file,date,xml_meta)
+
 # Band 13 is SCL, let's hope the order is always the same.
 
+# Reclassification matrix, classes 0,1,8,9,10 are no good
 reclass_matrix <- matrix(data=c(0,0,
                                 1,0,
                                 2,1,
@@ -55,16 +59,16 @@ reclass_matrix <- matrix(data=c(0,0,
                                 10,0,
                                 11,1),nrow=12,ncol=2,byrow=TRUE)
 
+# Masking each product with reclassification
 masked_products_vrt <- c()
-
 for(vrt in products_vrt){
   mask_scl <- reclassify(vrt$bands.13,reclass_matrix)
   vrt[mask_scl == 0] <- NA
   masked_products_vrt <- c(masked_products_vrt,vrt)
 }
-
 rm(vrt, mask_scl)
 
+# File organization, rename bands if possible
 # At 10 m, this files can be found:
 
 # At 20 m, this files can be found:
@@ -86,22 +90,27 @@ rm(vrt, mask_scl)
 # band 14 - TCI
 # band 16 - WVP
 
+# Run the mosaicing using do.call
 rasters.mosaicargs <- masked_products_vrt
 rasters.mosaicargs$fun <- mean
 level3 <- do.call(mosaic, rasters.mosaicargs)   
 rm(rasters.mosaicargs)
 
+# Set the directory to output directory
 setwd(args[1])
 
-# Naming for Level 3
-# S2_MSIL3_FROMDATE_TODATE_R(RESOLUTION).gtiff
+# Naming contruct for Level 3
+# Example: S2_MSIL3_FROMDATE_TODATE_R060.tif
 dates_products <- as.Date(dates_products)
 max_date <- as.character(format.Date(max(dates_products),'%Y%m%d'))
 min_date <- as.character(format.Date(min(dates_products),'%Y%m%d'))
 res <- xres(level3)
-
 export_name = paste0('S2_MSIL3_',min_date,'_',max_date,'_R0',res,'.tif')
+
+# Gives error, says brick is not recognized
 writeRaster(level3,fname = export_name, format = 'GTiff', overwrite=TRUE, bandorder='BIL',options="INTERLEAVE=BAND",bylayer=T)
 
 removeTmpFiles(h=0)
 
+# Back to initial directory
+setwd(owd)

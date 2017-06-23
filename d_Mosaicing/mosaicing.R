336a84e528e5a8e571d2afdbc6dd07e87ccc155e
rodrigoalmeida94 <rodrigo.almeida@wur.nl>
2017-06-23 15:02:42 +0200

    New version with handling of files in directory

--- d_Mosaicing/mosaicing.R
+++ d_Mosaicing/mosaicing.R
@@ -14,7 +14,7 @@ library(raster)
 # Handles arguments passed by CLI
 
 args = commandArgs(trailingOnly=TRUE)
-#Rscript --vanilla mosaicing.R input_dir output_dir
+#Rscript --vanilla mosaicing.R input_dir output_dir resolution?
 
 # If no arguments are supplied
 if (length(args)!=2) {
@@ -29,10 +29,32 @@ products_vrt <- c()
 
 for(elem in products) {
   # DANGER RESOLUTION
-  granule_dir = paste0(elem,'/GRANULE/',substr(elem,8,60),'/IMG_DATA/R60m/*.jp2')
-  output_file = paste0(elem,'/GRANULE/',substr(elem,8,60),'/IMG_DATA/R60m/bands.vrt')
-  system(paste('gdalbuildvrt -separate',output_file,granule_dir))
+  granule = dir(paste0(elem,'/GRANULE/'))
+  granule_dir = paste0(elem,'/GRANULE/',granule,'/IMG_DATA/R60m/*.jp2')
+  output_file = paste0(elem,'/GRANULE/',granule,'/IMG_DATA/R60m/bands.vrt')
+  system(paste('gdalbuildvrt -separate',output_file, granule_dir))
   products_vrt <- c(products_vrt,stack(output_file))
 }
 
+# Band 13 is SCL, let's hope the order is always the same.
 
+reclass_matrix <- matrix(data=c(0,0,
+                                1,0,
+                                2,1,
+                                3,1,
+                                4,1,
+                                5,1,
+                                6,1,
+                                7,1,
+                                8,0,
+                                9,0,
+                                10,0,
+                                11,1),nrow=12,ncol=2,byrow=TRUE)
+
+for(vrt in products_vrt){
+  mask_scl <- reclassify(vrt$bands.13,reclass_matrix)
+  vrt[mask_scl == 0] <- vrt$bands.1@file@nodatavalue
+}
+
+plot(products_vrt[[3]]$bands.10)
+plot(mask)

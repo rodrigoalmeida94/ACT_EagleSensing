4232576de47fcc0ab1134ed91b4832a82e3bff0a
arnanaraza <arnanaraza2006@gmail.com>
2017-06-23 12:24:32 +0200

    -added packages needed(lxml, shutil, multiprocesing, itertools) @ atmosphere envi -added a script to organize L1C and L2A files.

---
+++ b_Atmospheric_Correction/organize_L1C_L2A.py
@@ -0,0 +1,23 @@
+# Function to move L2A files from L1C since there's no argument to specify output files destination during sen2cor run.
+
+import shutil
+
+## Directory and L1C folder check
+datadir_L1C = r'/media/sf_M_DRIVE/L1C' #modify own directory
+print "The files in the data folder is/are: %s"%os.listdir(os.getcwd())
+
+## Make/set directory of L2A files and move files
+checker1 = "L2A"
+datadir_L2A = r'/media/sf_M_DRIVE/L2A' #modify own directory
+def folder_arrange (dir_L1C, dir_L2A):
+    dfiles = os.listdir(dir_L1C)
+    if not os.path.exists(dir_L2A):
+        os.makedirs(dir_L2A)
+    for folders in dfiles:
+        if folders [7:10] == checker1:
+            shutil.move(os.path.join(dir_L1C, folders), dir_L2A)
+            print "L2A folders successfully moved"
+        else:
+            print "no L2A folders yet"
+
+folder_arrange(datadir_L1C, datadir_L2A)

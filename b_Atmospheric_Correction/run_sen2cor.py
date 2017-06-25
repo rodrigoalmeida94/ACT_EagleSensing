b6d50f3220a57c6317fe2a0355a06229296fa303
arnanaraza <arnanaraza2006@gmail.com>
2017-06-25 09:39:24 -0400

    addressing overwriting issue after sen2cor parallel runs #1

--- b_Atmospheric_Correction/run_sen2cor.py
+++ b_Atmospheric_Correction/run_sen2cor.py
@@ -15,14 +15,15 @@ def run_sen2cor (res, dir): #Gets only L1C folders with .SAFE and process it
     os.chdir(dir)
     datafiles = os.listdir(os.getcwd())
     checker = "L1C"
+    L2A_only = ()
     for folders in datafiles:
         if folders[7:10] == checker and folders.endswith(".SAFE"):
             tfile = folders
             str(tfile)
             os.system ("L2A_Process --resolution="+ str(res) + " " + str(dir) + "/" + tfile)
-        else:
-            os.rename(folders, (folders + ".SAFE"))
-            print "folder renamed, re-run the script/function again."
+            L2A_only = os.listdir (dir)
+    return L2A_only
+
 
 
 # 2. BATCH PROCESSING
@@ -32,8 +33,9 @@ def sen2_batch (res, dir): # Creates a list of arguments based on number of file
     datafiles = os.listdir(os.getcwd())
     multiplier = len(datafiles)
     slist = [(res, dir)]*multiplier
-    pool = Pool(2) #adjust accordingly depending on computer processor capacity and files to be run
-    parmap.starmap(run_sen2cor, slist, pool=pool)
+    #pool = Pool(2) #adjust accordingly depending on computer processor capacity and files to be run
+    parmap.starmap(run_sen2cor, slist, parallel=True, processes=multiplier)
+
 
 datadir_L1C = r'/media/sf_M_DRIVE/L1C' #sample directory
 sen2_batch (10, datadir_L1C)

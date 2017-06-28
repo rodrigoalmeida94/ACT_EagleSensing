7b7baa8ab3caf26573e7e239cea8b1bb81fec905
keesbaake <kees.baake@gmail.com>
2017-06-28 16:30:58 +0200

    Update sen2cor_install.sh

--- z_Install_Software/sen2cor_install.sh
+++ z_Install_Software/sen2cor_install.sh
@@ -32,6 +32,8 @@ echo "export GDAL_DATA=\"${defdir}/anaconda2/envs/atmosphere/lib/python2.7/site-
 source $HOME/.bashrc
 
 ## Install additional packages
+conda install -c conda-forge fire
+
 pip install parmap
 pip install lxml
 pip install shutil

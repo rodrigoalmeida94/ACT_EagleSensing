2fbe04ac3f31aa714c310eb7d353a8380ddd79df
rodrigoalmeida94 <rodrigo.almeida@wur.nl>
2017-06-28 10:52:03 +0200

    Added modular structure, only one product at a time, require added instead of libraries

--- d_Mosaicing/mosaicing.R
+++ d_Mosaicing/mosaicing.R
@@ -6,23 +6,20 @@
 #setwd("/media/sf_D_DRIVE/hubTue20Jun20171214/L2A/")
 
 # ---- Setting up ----
-# Conda environment as well? Or just install packages?
-library(rgdal)
-library(utils)
-library(raster)
-library(XML)
-library(tools)
+# Libraries
+if(!require(rgdal)){install.packages('rgdal')}
+if(!require(utils)){install.packages('utils')}
+if(!require(raster)){install.packages('raster')}
+if(!require(XML)){install.packages('XML')}
+if(!require(tools)){install.packages('tools')}
 
-mask_dir <- function(mask_file,input_file_list) {
-  for(a_file in input_file_list) {
-    masked_file <- paste0(dirname(a_file),'/MSK_',basename(file_path_sans_ext(a_file)),'.tif')
-    system(paste0('gdal_calc.py -A "', mask_file,'" -B "',a_file,'" --outfile="',masked_file,'" --calc="A*B" --overwrite'))
-  }
-}
+# Sources
+source('func_mosaicing.R')
 
 # Handles arguments passed by CLI
 args = commandArgs(trailingOnly=TRUE)
-#Rscript --vanilla mosaicing.R input_dir output_dir resolution?, works perfectly
+# Usage
+# Rscript --verbose --default-packages= mosaicing.R INPUT_DIR OUTPUT_DIR
 
 # If no arguments are supplied
 if (length(args)<1) {
@@ -36,12 +33,13 @@ if(!dir.exists(args[1])){
   stop('Input L2A product directory does not exist.')
 }
 
-# Directory inputed by the user, where L2A products are, if no output, use the same as input
+# Directory inputed by the user, where L2A products are
 setwd(args[1])
 
 # ---- Load products into VRT ----
 # Get list of products in the directory
 products <- dir(pattern='*.SAFE')
+dates_products <- c()
 
 if(length(products)==0){
   stop('Input L2A product directory is empty or does not contain files in the .SAFE format.')
@@ -49,44 +47,6 @@ if(length(products)==0){
 if(length(products)==1){
   warning('Input L2A product directory contains only 1 file. Procedding with the masking procedure.')
 }
-
-# Load products into VRT, save dates from metadata
-products_vrt60 <- c()
-products_vrt20 <- c()
-products_vrt10 <- c()
-dates_products <- c()
-for(elem in products) {
-  granule = dir(paste0(elem,'/GRANULE/'))
-  granule_dir60 = paste0(elem,'/GRANULE/',granule,'/IMG_DATA/R60m/')
-  output_file60 = paste0(elem,'/bands60.vrt')
-  granule_dir20 = paste0(elem,'/GRANULE/',granule,'/IMG_DATA/R20m/')
-  output_file20 = paste0(elem,'/bands20.vrt')
-  granule_dir10 = paste0(elem,'/GRANULE/',granule,'/IMG_DATA/R10m/')
-  output_file10 = paste0(elem,'/bands10.vrt')
-  
-  if(dir.exists(granule_dir60)){
-    granule_dir60 = paste0(granule_dir60,'*.jp2')
-    system(paste('gdalbuildvrt -separate',output_file60, granule_dir60))
-    products_vrt60 <- c(products_vrt60,stack(output_file60))
-  }
-  
-  if(dir.exists(granule_dir20)){
-    granule_dir20 = paste0(granule_dir20,'*.jp2')
-    system(paste('gdalbuildvrt -separate',output_file20, granule_dir20))
-    products_vrt20 <- c(products_vrt20,stack(output_file20))
-  }
-  
-  if(dir.exists(granule_dir10)){
-    granule_dir10 = paste0(granule_dir10,'*.jp2')
-    system(paste('gdalbuildvrt -separate',output_file10, granule_dir10))
-    products_vrt10 <- c(products_vrt10,stack(output_file10))
-  }
-  
-  xml_meta <- xmlRoot(xmlParse(paste0(elem,'/MTD_MSIL2A.xml')))
-  date <- xmlValue(xml_meta[[1]][[1]][[1]])
-  dates_products <- c(dates_products,date)
-}
-rm(elem, granule, granule_dir60,output_file60, granule_dir20,output_file20,granule_dir10,output_file10,date,xml_meta)
 
 # ---- Reclassification of SCL, mask creation ----
 # Band 11 is SCL at R20m, let's hope the order is always the same.
@@ -147,103 +107,15 @@ reclass_matrix <- matrix(data=c(0,0,
                                 10,0,
                                 11,1),nrow=12,ncol=2,byrow=TRUE)
 
-# Creates mask files - WARNING - LONG RUN TIMES
-for(i in seq(length(products))){
-  if(length(products_vrt60)!=0){
-    mask_scl <- reclassify(products_vrt60[[i]]$bands60.13,reclass_matrix, filename=paste0(products[i],'/mask60.tif'),format='GTiff',datatype='INT2U',overwrite=T)
-  }
-  if(length(products_vrt10)!=0){
-    mask_scl <- reclassify(products_vrt20[[i]]$bands20.11,reclass_matrix,filename=paste0(products[i],'/mask20.tif'),format='GTiff',datatype='INT2U',overwrite=T)
-    mask_scl10 <- resample(mask_scl,products_vrt10[[i]]$bands10.1,method='ngb',filename=paste0(products[i],'/mask10.tif'),format='GTiff',datatype='INT2U',overwrite=T)
-  }
-  if(length(products_vrt10)==0 && length(products_vrt20)!=0){
-    mask_scl <- reclassify(products_vrt20[[i]]$bands20.11,reclass_matrix,filename=paste0(products[i],'/mask20.tif'),format='GTiff',datatype='INT2U',overwrite=T)
-  }
-}
-rm(i,mask_scl,mask_scl10)
-
-# ---- Masking of all files in products ----
-# Masks every file with the computed cloud mask
-for(prod in products) {
-  print(paste('Starting masking of',prod,'...'))
-  granule = dir(paste0(prod,'/GRANULE/'))
-  if(length(products_vrt60)!=0) {
-    files_60 <- list.files(paste0(prod,'/GRANULE/',granule,'/IMG_DATA/R60m'),pattern=glob2rx('*.jp2'), full.names=T)
-    mask_60 <- paste0(prod,'/mask60.tif')
-    mask_dir(mask_60,files_60)
-  }
-  if(length(products_vrt10)!=0) {
-    files_10 <- list.files(paste0(prod,'/GRANULE/',granule,'/IMG_DATA/R10m'),pattern=glob2rx('*.jp2'), full.names=T)
-    mask_10 <- paste0(prod,'/mask10.tif')
-    mask_dir(mask_10,files_10)
-    files_20 <- list.files(paste0(prod,'/GRANULE/',granule,'/IMG_DATA/R20m'),pattern=glob2rx('*.jp2'), full.names=T)
-    mask_20 <- paste0(prod,'/mask20.tif')
-    mask_dir(mask_20,files_20)
-  }
-  if(length(products_vrt10)==0 && length(products_vrt20)!=0) {
-    files_20 <- list.files(paste0(prod,'/GRANULE/',granule,'/IMG_DATA/R20m'),pattern=glob2rx('*.jp2'), full.names=T)
-    mask_20 <- paste0(prod,'/mask20.tif')
-    mask_dir(mask_20,files_20)
-  }
-  print(paste('Finished masking of',prod,'.'))
+for(prod in products){
+  dates_products <- c(dates_products,pre_process(prod,reclass_matrix))
 }
 
-rm(products_vrt10,products_vrt20,products_vrt60, files_10, mask_10,files_20,mask_20,files_60,mask_60)
-
-# ---- Loading masked products to VRT ----
-# Load masked products into VRT
-masked_products_vrt60 <- c()
-masked_products_vrt20 <- c()
-masked_products_vrt10 <- c()
-for(elem in products) {
-  granule = dir(paste0(elem,'/GRANULE/'))
-  granule_dir60 = paste0(elem,'/GRANULE/',granule,'/IMG_DATA/R60m/')
-  output_file60 = paste0(elem,'/masked_bands60.vrt')
-  granule_dir20 = paste0(elem,'/GRANULE/',granule,'/IMG_DATA/R20m/')
-  output_file20 = paste0(elem,'/masked_bands20.vrt')
-  granule_dir10 = paste0(elem,'/GRANULE/',granule,'/IMG_DATA/R10m/')
-  output_file10 = paste0(elem,'/masked_bands10.vrt')
-  
-  if(dir.exists(granule_dir60)){
-    granule_dir60 = paste0(granule_dir60,'MSK_*.tif')
-    system(paste('gdalbuildvrt -separate',output_file60, granule_dir60))
-    masked_products_vrt60 <- c(masked_products_vrt60,stack(output_file60))
-  }
-  
-  if(dir.exists(granule_dir20)){
-    granule_dir20 = paste0(granule_dir20,'MSK_*.tif')
-    system(paste('gdalbuildvrt -separate',output_file20, granule_dir20))
-    masked_products_vrt20 <- c(masked_products_vrt20,stack(output_file20))
-  }
-  
-  if(dir.exists(granule_dir10)){
-    granule_dir10 = paste0(granule_dir10,'MSK_*.tif')
-    system(paste('gdalbuildvrt -separate',output_file10, granule_dir10))
-    masked_products_vrt10 <- c(masked_products_vrt10,stack(output_file10))
-  }
-}
-rm(elem, granule, granule_dir60,output_file60, granule_dir20,output_file20,granule_dir10,output_file10)
-
-# ---- Passes 0 to NA ----
-# At 60 m
-if(length(masked_products_vrt60)!=0) {
-  for(i in 1:length(masked_products_vrt60)){
-      masked_products_vrt60[[i]][masked_products_vrt60[[i]]==0] <- NA
-    }
-}
-# At 10 m
-if(length(masked_products_vrt10)!=0) {
-  for(i in 1:length(masked_products_vrt10)){
-    masked_products_vrt10[[i]][masked_products_vrt10[[i]]==0] <- NA
-  }
-}
-# At 20 m
-if(length(masked_products_vrt20)!=0) {
-  for(i in 1:length(masked_products_vrt20)){
-    masked_products_vrt20[[i]][masked_products_vrt20[[i]]==0] <- NA
-  }
-}
-rm(i)
+# Returns date
+# Files are at product dir in
+# product_dir +'/MSK_60.tif'
+# product_dir +'/MSK_20.tif'
+# product_dir +'/MSK_10.tif'
 
 # ---- Reproject stacks to same CRS if different ----
 # Handle different CRS or use merge

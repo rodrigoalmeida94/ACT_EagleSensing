fee22d25e4a5813be5a209e8f3ed6af9f105e91c
Rodrigo Almeida <rodrigo.almeida94@outlook.pt>
2017-06-30 16:21:30 +0200

    Update download_hub.py

--- a_Data_Acquisition/download_hub.py
+++ a_Data_Acquisition/download_hub.py
@@ -63,7 +63,7 @@ def download_hub(download_dir,
     os.chdir(owd)
 
     return new_dir
-    text_file = open('TIME_DIR.txt','w')
+    text_file = open(download_dir+'TIME_DIR.txt','w')
     text_file.write(new_dir)
     text_file.close()
 
